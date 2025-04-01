3f121271e2d82c7778b8e23703b619aadefe0a53
Cantexinho <pk.developing@gmail.com>
2025-04-01 15:43:16 +0300

    Finished initial implementation

--- src/naive_bayes_knn_algorithms/preprocessor.py
+++ src/naive_bayes_knn_algorithms/preprocessor.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import logging
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
@@ -7,15 +8,17 @@ from scipy.sparse import csr_matrix
 
 from naive_bayes_knn_algorithms.models.train_test import TrainTestData
 
+_logger = logging.getLogger(__name__)
+
 
 class PreProcessor:
-    def __init__(self):
-        self._vectorizer = TfidfVectorizer(stop_words="english")
+    def __init__(self, vectorizer: TfidfVectorizer = None):
+        self._vectorizer = vectorizer
         self._df = None
 
     def load_data(self, file_path: str) -> pd.DataFrame:
         """Load the dataset from the specified file path."""
-
+        _logger.info("Loading data from file.")
         try:
             self._df = pd.read_csv(file_path, encoding="ISO-8859-1")
         except FileNotFoundError:
@@ -30,6 +33,7 @@ class PreProcessor:
         self, vectorized_text: csr_matrix, labels: pd.Series
     ) -> TrainTestData:
         """Split the data into training and testing sets."""
+        _logger.info("Splitting data into training and testing sets.")
 
         if self._df is None:
             raise ValueError("Data not loaded. Call load_data() first.")
@@ -47,6 +51,7 @@ class PreProcessor:
 
     def preprocess_data(self) -> TrainTestData:
         """Preprocess the data by renaming columns."""
+        _logger.info("Preprocessing data.")
 
         if self._df is None:
             raise ValueError("Data not loaded. Call load_data() first.")
@@ -59,3 +64,11 @@ class PreProcessor:
         split_train_test_data = self._split_train_test(vectorized_text, labels)
 
         return split_train_test_data
+
+
+_pre_processor = PreProcessor(vectorizer=TfidfVectorizer(stop_words="english"))
+
+
+def get_preprocessor() -> PreProcessor:
+    """Get an instance of the PreProcessor."""
+    return _pre_processor
--- src/naive_bayes_knn_algorithms/training/model_trainer.py
+++ src/naive_bayes_knn_algorithms/training/model_trainer.py
@@ -1,4 +1,5 @@
 import joblib
+import logging
 
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
@@ -6,7 +7,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from scipy.sparse import csr_matrix
 import pandas as pd
 
-from naive_bayes_knn_algorithms.conf.root import settings
+from naive_bayes_knn_algorithms.conf.root import root_settings
+
+_logger = logging.getLogger(__name__)
 
 
 class ModelTrainer:
@@ -19,9 +22,12 @@ class ModelTrainer:
     def _save_model(self) -> None:
         """Save the trained model to a file."""
 
+        _logger.info(
+            "Saving model",
+        )
         joblib.dump(
             self.model,
-            settings.get_ml_models_save_path(self.model_name),
+            root_settings.get_ml_models_save_path(self.model_name),
         )
 
     def load_data(
@@ -32,6 +38,9 @@ class ModelTrainer:
         label_train: pd.Series,
     ) -> None:
         """Load the training data into the trainer."""
+        _logger.info(
+            "Loading data into trainer",
+        )
         self.model = model
         self.model_name = model_name
         self.text_train = text_train
@@ -39,8 +48,19 @@ class ModelTrainer:
 
     def train_model(self) -> MultinomialNB | KNeighborsClassifier:
         """Train the model with the provided training data."""
+        _logger.info(
+            "Training model",
+        )
 
         self.model.fit(self.text_train, self.label_train)
         self._save_model()
 
         return self.model
+
+
+model_trainer = ModelTrainer()
+
+
+def get_model_trainer() -> ModelTrainer:
+    """Get an instance of the ModelTrainer."""
+    return model_trainer
