df653272df05d056320b81f5a6b9fcacc9905898
Cantexinho <pk.developing@gmail.com>
2025-04-02 10:15:56 +0300

    Added Worker to run model training into main

--- src/naive_bayes_knn_algorithms/__main__.py
+++ src/naive_bayes_knn_algorithms/__main__.py
@@ -1,7 +1,8 @@
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 
-from naive_bayes_knn_algorithms.logging import setup_logging
+from naive_bayes_knn_algorithms.logging_setup import setup_logging
+from naive_bayes_knn_algorithms.conf.root import root_settings
 
 from naive_bayes_knn_algorithms.worker import Worker
 
@@ -11,12 +12,12 @@ setup_logging()
 def main():
     naive_bayes_worker = Worker(
         model=MultinomialNB(),
-        data_location="src/naive_bayes_knn_algorithms/data/spam.csv",
+        data_location=root_settings.dataset_path,
         model_name="naive_bayes_spam_detection_model_001",
     )
     knn_worker = Worker(
         model=KNeighborsClassifier(),
-        data_location="src/naive_bayes_knn_algorithms/data/spam.csv",
+        data_location=root_settings.dataset_path,
         model_name="multinomial_spam_detection_model_001",
     )
 
